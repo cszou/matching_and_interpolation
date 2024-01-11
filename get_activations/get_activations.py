35584c1a6f920906e8f71a089402aefdcf14d252
cszou <42100343+cszou@users.noreply.github.com>
2024-01-11 16:00:09 -0500

    update get_activations.py

--- get_activations/get_activations.py
+++ get_activations/get_activations.py
@@ -25,7 +25,7 @@ def get_activation(name):
     return hook
 
 
-def get_topk_dataset_loader(batch_size=256, num_workers=10, imagenet_dir="/data/imagenet_data/train"):
+def get_topk_dataset_loader(batch_size=256, num_workers=10, imagenet_dir="/data/imagenet_data"):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     standard_test_transform = transforms.Compose(
         [transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor(), normalize]
