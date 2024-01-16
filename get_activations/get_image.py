eab275b54dd43d0fac626c0a2b2ed25cad97e250
cszou <42100343+cszou@users.noreply.github.com>
2024-01-16 16:23:13 -0500

    update get_image.py

--- get_activations/get_image.py
+++ get_activations/get_image.py
@@ -11,12 +11,23 @@ class ImageNetWithIndices(datasets.ImageNet):
         return img, label, index
 
 
+
 top_indices = torch.load('m1.result.pth.tar')['top_dataset_indices']
 standard_test_transform = transforms.Compose(
         [transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor()]
     )
 imagenet_data = ImageNetWithIndices('/data/imagenet_data', split="train", transform=standard_test_transform)
-i = 0
-for v in top_indices.values():
-    torch.save(imagenet_data[v.transpose(0,1)[0][0]], f'pic_{i}')
-    i += 1
+images = {}
+for k,v in top_indices.items():
+    images[k] = {}
+    channel = 0
+    for top_channel_indices in v.transpose(0,1):
+        top_images = list()
+        for index in top_channel_indices:
+            top_images.append = imagenet_data[index][0]
+        images[k][f'channel_{channel}'] = top_images
+        channel += 1
+for k in images.keys():
+    print(f'{k}:', end=' ')
+    for c, v in images[k].keys():
+        print(f'{c}: {v.shape}')
