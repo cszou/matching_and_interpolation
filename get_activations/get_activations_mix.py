270fd29e6bafdd0c6cc9fbbf8a062ce82af16e1e
cszou <42100343+cszou@users.noreply.github.com>
2024-01-25 12:31:22 -0500

    fix get_activations_mix.py

--- get_activations/get_activations_mix.py
+++ get_activations/get_activations_mix.py
@@ -67,7 +67,7 @@ if __name__=='__main__':
     mixedModel = OrderedDict()
     # vanillaMatchedModel = OrderedDict()
     for key in m1p['state_dict'].keys():
-        mixedModel[key] = 0.5 * m1p['state_dict'][k] + 0.5 * m2p['state_dict'][k]
+        mixedModel[key] = 0.5 * m1p['state_dict'][key] + 0.5 * m2p['state_dict'][key]
         # vanillaMatchedModel[k] = a * 0.05 * m1['state_dict'][k] + (1-a*0.05) * m2o['state_dict'][k].to('cuda')
 
     model.load_state_dict(mixedModel)
