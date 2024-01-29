43870e45aa07b9858a3851884df6e90414d6ed2b
cszou <42100343+cszou@users.noreply.github.com>
2024-01-29 13:11:15 -0500

    test

--- get_activations/get_clip.py
+++ get_activations/get_clip.py
@@ -37,11 +37,13 @@ def get_clip_encodings_from_index_tensor(indices, topk=10,  batch_size = 256, nu
 if __name__ == '__main__':
     indices_m1 = torch.load('m1.result.pth.tar')['top_dataset_indices']
     # print(indices_m1)
+    embeddings_m1 = {}
     for k, v in indices_m1.items():
         print(k, v.shape)
     # indices_m2 = torch.load('m2.result.pth.tar')['top_dataset_indices']
-        embeddings_m1 = get_clip_encodings_from_index_tensor(v)
-        print(embeddings_m1.shape)
-        print(embeddings_m1[0, 0])
-        break
+        embeddings_m1[k] = get_clip_encodings_from_index_tensor(v)
+        print(embeddings_m1[k].shape)
+        # print(embeddings_m1[0, 0])
+        # break
+    print(embeddings_m1)
     # embeddings_m2 = get_clip_encodings_from_index_tensor(indices_m2)
