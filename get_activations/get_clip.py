f2ba4cde6150c8fe62c63fd2171719951d24b754
cszou <42100343+cszou@users.noreply.github.com>
2024-01-29 13:53:01 -0500

    cleanup

--- get_activations/get_clip.py
+++ get_activations/get_clip.py
@@ -5,10 +5,7 @@ from utils import *
 def get_clip_encodings_from_index_vector(indices, dataloader, model, clip_device):
     # print(f'shape of indices {indices.shape}')
     top_images = get_images_from_indices(indices, dataloader.dataset).squeeze() #Need to squeeze for the model
-    # top_images2 = get_images_from_indices2(indices, 1, dataloader).squeeze() #Need to squeeze for the model
-    # print(f'top_images shape: {top_images.shape}')
-    # print(top_images2.shape)
-    print(top_images.shape)
+    # print(top_images.shape)
     with torch.no_grad():
         image_features = model.encode_image(top_images.to(clip_device))
 
@@ -23,11 +20,9 @@ def get_clip_encodings_from_index_tensor(indices, topk=10,  batch_size = 256, nu
     dataloader = get_clip_encoding_dataloader(preprocess, batch_size, num_workers)
     print('grabbing embeddings of top images for all channels')
     all_embeddings = []
-    print(indices.shape)
     for index_of_top_kth_images in tqdm(range(topk)):
         top_kth_embedding = get_clip_encodings_from_index_vector(indices[index_of_top_kth_images].unsqueeze(0), dataloader, model, clip_device)
         all_embeddings.append(top_kth_embedding.unsqueeze(0))
-        # break
 
     embeddings_tensor = torch.vstack(all_embeddings)
     # top_kth_embedding = get_clip_encodings_from_index_vector(indices, dataloader, model, clip_device)
