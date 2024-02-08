1ad2863d9f4124ce4128971c0b5528f7e82d42a4
cszou <42100343+cszou@users.noreply.github.com>
2024-02-08 09:38:20 -0500

    update get_kt.py

--- get_activations/get_kt.py
+++ get_activations/get_kt.py
@@ -28,32 +28,12 @@ if __name__=='__main__':
                 for key, v in activations.items():
                     activations_norms = torch.linalg.matrix_norm(v)
                     print(key, activations_norms.shape)
-                    # if i == 0:
-                    #     norms, dataset_indices = torch.topk(activations_norms, k, dim=0)
-                        # print(norms.shape)
-        #             else:
-        #                 # For the current batch, get the top norms and their indices
-        #                 batch_norms, b_indices = torch.topk(activations_norms, k=k, dim=0)
-        #                 # Get the dataset indices corresponding to the batch_indices
-        #                 batch_dataset_indices = batch_indices[b_indices].to(device)
-        #
-        #                 # Need to stack the indices and norms we already have together, then sort and update the top ones
-        #                 norms_stack = torch.cat((top_norms[key], batch_norms))  # .detach()
-        #                 indices_stack = torch.cat((top_dataset_indices[key], batch_dataset_indices))
-        #
-        #                 # get the indices and values of the max norms
-        #                 norms, indices = torch.topk(norms_stack, k=k, dim=0)
-        #
-        #
-        #                 # gather the dataset indices from the indices stack
-        #                 dataset_indices = torch.gather(indices_stack, 0, indices)
-        #             top_norms[key] = norms
-        #             top_dataset_indices[key] = dataset_indices
-        # print(f'Top images for {model_name} found!')
-        # for key, value in top_norms.items():
-        #     print('Layer: ', key)
-        #     print('\tTop image activation norms: ', top_norms[key].shape)
-        #     print('\tTop indices shape : ', top_dataset_indices[key].shape)
+                    if i == 0:
+                        norms[key] = [activations_norms]
+                    else:
+                        norms[key].append(activations_norms)
+        for k, v in norms.items():
+            print(k, v)
         # torch.save({'top_norms': top_norms,
         #             'top_dataset_indices': top_dataset_indices}, model_name+'.result.pth.tar')
 
