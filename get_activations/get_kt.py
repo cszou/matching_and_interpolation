ceff1822080104bd5c5765d515cb982b3d26ce08
cszou <42100343+cszou@users.noreply.github.com>
2024-02-14 02:18:26 -0500

    test

--- get_activations/get_kt.py
+++ get_activations/get_kt.py
@@ -4,12 +4,12 @@ from scipy import stats
 models = ['m1', 'm2']
 layers = {'0': 64, '3': 192, '6': 384, '8': 256, '10': 256}
 for k,v in layers.items():
-    ranks_1 = torch.load(f'm1_ranks_features_{k}.result.pth.tar')
-    ranks_2 = torch.load(f'm2_ranks_features_{k}.result.pth.tar')
+    ranks_1 = torch.load(f'./activations_results/m1_activations_features_{k}.result.pth.tar')
+    ranks_2 = torch.load(f'./activations_results/m1_activations_features_{k}.result.pth.tar')
     for i in range(layers[k]):
         if i > 64:
             break
         print(f'ranks shape: {ranks_1.shape}')
-        res = stats.kendalltau(ranks_1[i], ranks_2[i])
+        res = stats.kendalltau(ranks_1[i].transpose(0,1), ranks_2[i].transpose(0,1))
         corr = res.correlation
         print(f'feature{k}_layer{i} correlation: {corr}')
