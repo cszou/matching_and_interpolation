64aa7abbc9013fde1ae09b8579adeeb8229aa488
cszou <42100343+cszou@users.noreply.github.com>
2024-02-13 22:49:25 -0500

    update get_kt.py

--- get_activations/get_kt.py
+++ get_activations/get_kt.py
@@ -7,8 +7,9 @@ for k,v in layers.items():
     ranks_1 = torch.load(f'm1_ranks_features_{k}.result.pth.tar')
     ranks_2 = torch.load(f'm2_ranks_features_{k}.result.pth.tar')
     for i in range(layers[k]):
-        # print(f'ranks shape: {ranks_1.shape}')
+        if i > 64:
+            break
+        print(f'ranks shape: {ranks_1.shape}')
         res = stats.kendalltau(ranks_1[i], ranks_2[i])
         corr = res.correlation
         print(f'feature{k}_layer{i} correlation: {corr}')
-        # break
